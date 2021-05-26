fcdbd40f566dea30a95ccc6b575998679cd53cae
aliza <>
2021-05-26 22:52:02 +0300

    Add more files

--- src/primitives/Color.java
+++ src/primitives/Color.java
@@ -5,7 +5,7 @@ package primitives;
  * non-negative RGB values. The colors are maintained without upper limit of
  * 255. Some additional operations are added that are useful for manipulating
  * light's colors
- * 
+ *
  * @author Dan Zilberstein
  */
 public class Color {
@@ -13,20 +13,28 @@ public class Color {
 	 * The internal fields tx`o maintain RGB components as double numbers from 0 to
 	 * whatever...
 	 */
-	private final double r;
-	private final double g;
-	private final double b;
+	private double r = 0.0;
+	private double g = 0.0;
+	private double b = 0.0;
 
-	/**
-	 * Black color = (0,0,0)
-	 */
 	public static final Color BLACK = new Color();
+	public static final Color BLUE = new Color(0, 0, 255);
+	public static final Color RED = new Color(255, 0, 0);
+	public static final Color GREEN = new Color(0, 255, 0);
+	public static final Color YELLOW = new Color(255, 255, 0);
+	public static final Color CYAN = new Color(0, 255, 255);
+	public static final Color MAGENTA = new Color(255, 0, 255);
+	public static final Color ORANGE = new Color(255, 200, 0);
+	public static final Color PINK = new Color(255, 175, 175);
+	public static final Color DARKGREY = new Color(64, 64, 64);
+	public static final Color GREY = new Color(128, 128, 128);
+	public static final Color LIGHTGREY = new Color(192, 192, 192);
+	public static final Color WHITE = new Color(255, 255, 255);
 
 	/**
 	 * Default constructor - to generate Black Color (privately)
 	 */
 	private Color() {
-		r = g = b = 0.0;
 	}
 
 	/**
@@ -47,7 +55,7 @@ public class Color {
 
 	/**
 	 * Copy constructor for Color
-	 * 
+	 *
 	 * @param other the source color
 	 */
 	public Color(Color other) {
@@ -58,13 +66,69 @@ public class Color {
 
 	/**
 	 * Constructor on base of java.awt.Color object
-	 * 
+	 *
 	 * @param other java.awt.Color's source object
 	 */
 	public Color(java.awt.Color other) {
 		r = other.getRed();
 		g = other.getGreen();
 		b = other.getBlue();
+	}
+
+	/**
+	 * Color setter to reset the color to BLACK
+	 *
+	 * @return the Color object itself for chaining calls
+	 */
+	public Color setColor() {
+		r = 0.0;
+		g = 0.0;
+		b = 0.0;
+		return this;
+	}
+
+	/**
+	 * Color setter to generate a color according to RGB components Each component
+	 * in range 0..255 (for printed white color) or more [for lights]
+	 *
+	 * @param r Red component
+	 * @param g Green component
+	 * @param b Blue component
+	 * @return the Color object itself for chaining calls
+	 */
+	public Color setColor(double r, double g, double b) {
+		if (r < 0 || g < 0 || b < 0)
+			throw new IllegalArgumentException("Negative color component is illegal");
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		return this;
+	}
+
+	/**
+	 * Color setter to copy RGB components from another color
+	 *
+	 * @param other source Color object
+	 * @return the Color object itself for chaining calls
+	 */
+	public Color setColor(Color other) {
+		r = other.r;
+		g = other.g;
+		b = other.b;
+		return this;
+	}
+
+	/**
+	 * Color setter to take components from an base of java.awt.Color object
+	 *
+	 * @param other java.awt.Color's source object
+	 * @return the Color object itself for chaining calls
+	 */
+	public Color setColor(java.awt.Color other) {
+		r = other.getRed();
+		g = other.getGreen();
+		b = other.getBlue();
+		return this;
 	}
 
 	/**
@@ -112,7 +176,7 @@ public class Color {
 
 	/**
 	 * Scale the color by (1 / reduction factor)
-	 * 
+	 *
 	 * @param k reduction factor
 	 * @return new Color object which is the result of the operation
 	 */
